7008ab85f9d7f30826fed2263c78fe98d2f5cf65
bill.lin <zzx123bill@gmail.com>
2022-01-20 22:10:46 +0800

    setup deposit and withdraw fake data

---
+++ quarkus_kafka/deposit.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import {check, sleep} from 'k6';
+
+const incomePayload = JSON.parse(open("../deposit.json"));
+
+export default function() {
+  const url = 'http://localhost:8080/transactions';
+
+  const params = {
+	headers: {
+	  'Content-Type': 'application/json',
+	},
+  };
+
+  const res = http.post(url, JSON.stringify(incomePayload), params);
+
+  check(res, {
+	 'success creating income': (r) => r.status === 201 
+	});
+  sleep(1);
+}
